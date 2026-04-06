9aa0e7c1173ad01bda7f3a0f6b77fe76107eed32
jeam-cloud <142361204+Jeam-pixel@users.noreply.github.com>
2026-04-06 13:28:48 -0700

    added footstep audio

--- Assets/Scripts/MouseFootsteps.cs
+++ Assets/Scripts/MouseFootsteps.cs
@@ -2,15 +2,65 @@ using UnityEngine;
 
 public class MouseFootsteps : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("Audio Clips")]
+    public AudioClip[] footstepSounds;
+    public AudioClip[] squeakSounds;
+
+    [Header("Settings")]
+    public float stepInterval = 0.4f;
+    public float squeakChance = 0.08f;  // 8% chance of squeak per step
+    public float moveThreshold = 0.02f;
+
+    private AudioSource audioSource;
+    private float stepTimer = 0f;
+    private Vector3 lastPosition;
+
     void Start()
     {
-        
+        audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource.spatialBlend = 0f;
+        audioSource.volume = 0.5f;
+        lastPosition = transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Vector3 currentFlat = new Vector3(transform.position.x, 0, transform.position.z);
+        Vector3 lastFlat = new Vector3(lastPosition.x, 0, lastPosition.z);
+        float distanceMoved = Vector3.Distance(currentFlat, lastFlat);
+
+        bool isMoving = distanceMoved > moveThreshold * Time.deltaTime;
+
+        if (isMoving)
+        {
+            stepTimer -= Time.deltaTime;
+            if (stepTimer <= 0f)
+            {
+                PlayStep();
+                stepTimer = stepInterval;
+            }
+        }
+        else
+        {
+            stepTimer = 0f;
+        }
+
+        lastPosition = transform.position;
+    }
+
+    void PlayStep()
+    {
+        if (squeakSounds.Length > 0 && Random.value < squeakChance)
+        {
+            AudioClip squeak = squeakSounds[Random.Range(0, squeakSounds.Length)];
+            audioSource.PlayOneShot(squeak, 0.6f);
+            return;
+        }
+
+        if (footstepSounds.Length > 0)
+        {
+            AudioClip step = footstepSounds[Random.Range(0, footstepSounds.Length)];
+            audioSource.PlayOneShot(step, 0.4f);
+        }
     }
 }
