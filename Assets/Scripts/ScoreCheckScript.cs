a707c51b3e4dab526a9babf8bedaf552204d6b97
KaleBale <KabyangaPaul@gmail.com>
2026-04-06 19:27:34 +0300

    score check and path opening
    Added score checking and path opening functionality along with text that shows when score is reached

--- Assets/Scripts/ScoreCheckScript.cs
+++ Assets/Scripts/ScoreCheckScript.cs
@@ -9,6 +9,8 @@ public class ScoreCheckScript : MonoBehaviour
     public GameObject targetObject1;
     public GameObject targetObject2;
     public TextMeshProUGUI Text;
+
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
    void Awake()
@@ -17,15 +19,23 @@ public class ScoreCheckScript : MonoBehaviour
     }
     void Start()
     {
-        
+        Text.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         if (ScoreManager.instance.currentPoints >= 15){
-            Destroy(targetObject2)
-            Text.text = "Mousehole Open!"; 
+            Destroy(targetObject2);
+            Text.gameObject.SetActive(true);
+            Text.text = "You got 15 points! Mousehole is Open!"; 
+            Invoke(nameof(HideText), 2f);
+        }
+
+        if (ScoreManager.instance.currentPoints >= 30){
+            Destroy(targetObject1);
+            Text.gameObject.SetActive(true);
+            Text.text = "You got 30 points! Vent is Open!"; 
             Invoke(nameof(HideText), 2f);
         }
         
