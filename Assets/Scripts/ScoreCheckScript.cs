d9aba10f8cd430ac058e3f3bbb5e459a790c22b8
KaleBale <KabyangaPaul@gmail.com>
2026-04-07 03:33:52 +0300

    collision check

--- Assets/Scripts/ScoreCheckScript.cs
+++ Assets/Scripts/ScoreCheckScript.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 using TMPro;
-using System.Net.Mime;
+using System.Dynamic;
 
 public class ScoreCheckScript : MonoBehaviour
 {    
@@ -9,6 +9,9 @@ public class ScoreCheckScript : MonoBehaviour
     public GameObject targetObject1;
     public GameObject targetObject2;
     public TextMeshProUGUI Text;
+
+    private bool message15Shown = false;
+    private bool message30Shown = false;
 
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -25,17 +28,38 @@ public class ScoreCheckScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (ScoreManager.instance.currentPoints >= 15){
+        if (ScoreManager.instance.currentPoints >= 15 && !message15Shown){
+            message15Shown = true;
             Destroy(targetObject2);
             Text.gameObject.SetActive(true);
             Text.text = "You got 15 points! Mousehole is Open!"; 
             Invoke(nameof(HideText), 2f);
         }
 
-        if (ScoreManager.instance.currentPoints >= 30){
+        if (ScoreManager.instance.currentPoints >= 30 && !message30Shown){
+            message30Shown = true;
             Destroy(targetObject1);
             Text.gameObject.SetActive(true);
             Text.text = "You got 30 points! Vent is Open!"; 
+            Invoke(nameof(HideText), 2f);
+        }
+
+
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == targetObject1)
+        {
+            Text.gameObject.SetActive(true);
+            Text.text = "Can't go here unless you've got 30 points!";
+            Invoke(nameof(HideText), 2f);
+        } 
+        else if (other.gameObject == targetObject2)
+        {
+            Text.gameObject.SetActive(true);
+            Text.text = "Can't go here unless you've got 15 points!";
             Invoke(nameof(HideText), 2f);
         }
         
